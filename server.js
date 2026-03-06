369346a851ba79958d6d4397330912da7c56f3c4
MilanVanSteen <537674@student.fontys.nl>
2026-03-06 12:32:16 +0100

    Auto stash before merge of "release" and "origin/release"

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname)));
-
-// SPA fallback (optional, if you plan client-side routing)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.get("*", (req, res) => {res.sendFile(path.join(__dirname, "index.html"));});
 
 app.listen(PORT, () => console.log(`Frontend running on port ${PORT}`));
